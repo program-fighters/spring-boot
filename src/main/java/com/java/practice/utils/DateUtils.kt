a3272be1dc13c7d7153cb0ca7221904d065bca1a
Sanjeev Jha <sanjeev.jha@impressico.com>
2019-02-19 18:02:37 +0530

    add one method

--- src/main/java/com/java/practice/utils/DateUtils.kt
+++ src/main/java/com/java/practice/utils/DateUtils.kt
@@ -1,5 +1,11 @@
 package com.java.practice.utils
 
+import java.util.*
+
 fun currentTimeInMills(): Long {
     return System.currentTimeMillis()
+}
+
+fun currentDate(): Date {
+    return Date()
 }
